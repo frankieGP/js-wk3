1df270b69f72290d59430229ddeabc1bc38e5362
Frankie Gavin <frankanthonygavin@gmail.com>
2017-01-20 10:45:59 -0800

    init commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>To Do List</h1>
+  </div>  `
+})
+
+export class AppComponent {
+
+}
